3df4ea3d2739a4b27d314c094c8751608b55e979
Eugen Selbstreich <qduck@mail.ru>
2024-04-27 22:41:28 +0200

    feat: finaly

--- scripts/Deck.gd
+++ scripts/Deck.gd
@@ -1,5 +1,22 @@
 extends TextureButton
 
+enum PiggyType {
+	Predator = 0,
+	Einstein = 1,
+	Phoebe = 2,
+	Ciri = 3,
+	Legolas = 4,
+	Triss = 5,
+	Emma = 6,
+	Tauriel = 7,
+	Arya = 8,
+	Teddy = 9,
+	Nackedei = 10
+}
+
+var Cards = [
+	{type = PiggyType.Einstein, value = 1}
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,4 +29,21 @@ func _process(delta):
 
 
 func _on_pressed():
-	print("Deck pressed")
+	$"../../GridContainer".add_child(CreateCard(PiggyType.Phoebe, 2))
+
+func CreateCard(piggyType: int, cardValue: int) -> Node:
+	var marginContainer = MarginContainer.new()
+	var textureRect = TextureRect.new()
+	var texturePathPreset := "res://assets/%s%s.png"
+	var texturePath = texturePathPreset % [PiggyType.keys()[piggyType], cardValue]
+	
+	if piggyType == 0 or cardValue == 0:
+		texturePath = "res://assets/Predator.png"
+	
+	textureRect.texture = load(texturePath)
+	textureRect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+	marginContainer.add_child(textureRect)
+	marginContainer.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	marginContainer.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	return marginContainer
+	
