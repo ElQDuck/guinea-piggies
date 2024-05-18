40e454a405cdd286d9f2d8c8225bf8d0cf86cfdc
Eugen Selbstreich <qduck@mail.ru>
2024-05-19 00:50:19 +0200

    feat: add

---
+++ scripts/profile_setup_player.gd
@@ -0,0 +1,14 @@
+extends Control
+
+@export var ReadyButton: Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# Adjust the pivot to make rotations around the center
+	self.pivot_offset = self.get_size() / 2
