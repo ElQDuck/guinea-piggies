938f6113b11fc35db578a28bd9a3bc7ea103c489
Eugen Selbstreich <qduck@mail.ru>
2024-05-19 23:36:00 +0200

    feat: add button

--- scripts/profile_setup.gd
+++ scripts/profile_setup.gd
@@ -3,7 +3,7 @@ extends Control
 @export var player_1: Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player_1.ReadyButton.pressed.connect(_toggled_ready_button.bind("blub"))
+	player_1.ready_button.pressed.connect(_toggled_ready_button.bind("blub"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/profile_setup_player.gd
+++ scripts/profile_setup_player.gd
@@ -1,14 +1,25 @@
 extends Control
 
-@export var ReadyButton: Button
+@export var color_preview: Panel
+@export var color_button_left: Button
+@export var color_button_right: Button
+@export var avatar_image: TextureRect
+@export var avatar_button_left: Button
+@export var avatar_button_right: Button
+@export var input_player_name: LineEdit
+@export var ready_button: Button
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	color_button_left.pressed.connect(_on_color_button_left_pressed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# Adjust the pivot to make rotations around the center
 	self.pivot_offset = self.get_size() / 2
+
+
+func _on_color_button_left_pressed():
+	print("left")
