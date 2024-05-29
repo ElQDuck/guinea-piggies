f6982b5d51ce287775c780bf72643c4e6a4ef003
Eugen Selbstreich <qduck@mail.ru>
2024-05-30 01:17:39 +0200

    feat: nice animations

--- scenes/dice_selection.gd
+++ scenes/dice_selection.gd
@@ -30,13 +30,31 @@ func _handle_lofty_selection(toggled_on: bool, selection: int):
 func _handle_start_wheel_button_pressed():
 	# Make the dice buttons invisible
 	dice_select_panel.set_visible(false)
+	
 	# Resize to 0 to scale up with tween to get a pop up effect
 	fortune_wheel.set_scale(Vector2(0, 0))
 	fortune_wheel.set_visible(true)
-
-	# Scaling up with tween
+	
+	# Scaling up
 	var scale_up_tween = create_tween()
 	scale_up_tween.tween_property(fortune_wheel, "scale", Vector2(1, 1), 0.7).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
-
+	
+	# Get random number for wheel result
+	var random_num = randi() % 6 + 1
+	
 	# Spin the wheel
-	fortune_wheel.spin(3)
+	fortune_wheel.spin(random_num)
+	await get_tree().create_timer(5.5).timeout
+	
+	# Scaling down
+	var scale_down_tween = create_tween()
+	scale_down_tween.tween_property(fortune_wheel, "scale", Vector2(0, 0), 0.7).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
+	await scale_down_tween.finished
+	
+	# Reset this UI to the initial visibility
+	dice_select_panel.set_visible(true)
+	fortune_wheel.set_visible(false)
+	self.set_visible(false)
+	
+	# Trigger a signal with the wheel result
+	# TODO
