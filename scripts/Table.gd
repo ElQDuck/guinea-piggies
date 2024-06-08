0a9c5d3d3ad1b55b8efb37f47bcfc24f58d37de3
Eugen Selbstreich <qduck@mail.ru>
2024-06-08 23:59:24 +0200

    feat: add double cards check

--- scripts/Table.gd
+++ scripts/Table.gd
@@ -26,6 +26,34 @@ func _place_card_on_table(card: Card):
 	var cards_area_center_position: Vector2 = drawn_cards_area.get_global_position()
 	move_card(played_card, deck_position, cards_area_center_position)
 	played_card.flip_card(3)
+	_check_double_cards()
+
+
+func _check_double_cards():
+	if cards_on_table.size() > 1:
+		var last_card = cards_on_table.back()
+		# Find double card types
+		for i in range(cards_on_table.size() - 1):
+			if cards_on_table[i].type == last_card.type:
+				print("Double card found: " + Card.PiggyType.keys()[last_card.type] + " at " + str(i))
+				break
+
+#func CheckPredator(newCard: Card):
+	#if newCard.Type == Card.PiggyType.Predator:
+		#print("Predator!")
+		#CleanupTable()
+		#if Player1.Active and Player2.CardsInHand.size() > 0:
+			#$"../DiceSelectionUi/VBoxContainer/Label".text = "Player 1 Choose the number of cards you want to steal from Player 2."
+			#$"../DiceSelectionUi".set_visible(true)
+		#if Player1.Active and Player2.CardsInHand.size() == 0:
+			#print("Player has no cards in hand. Skip predator event.")
+			#SwitchPlaerTurn()
+		#if Player2.Active and Player1.CardsInHand.size() > 0:
+			#$"../DiceSelectionUi/VBoxContainer/Label".text = "Player 2 Choose the number of cards you want to steal from Player 1."
+			#$"../DiceSelectionUi".set_visible(true)
+		#if Player2.Active and Player1.CardsInHand.size() == 0:
+			#print("Player has no cards in hand. Skip predator event.")
+			#SwitchPlaerTurn()
 
 
 func move_card(card_in_scene: Panel, from: Vector2, to: Vector2):
